8b1dc412316bbb71da11b3e367846dfbbd944b09
Nitish Victor <nithishvictor@gmail.com>
2017-09-26 11:24:43 -0400

    App.js fix

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
+var express = require('express');
+var cors = require('cors');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(cors())
-app.use(express.static(__dirname + '/'))
+app.set('port', (process.env.PORT || 5000));
+app.use(cors());
+app.use(express.static(__dirname + '/'));
 
 // app.get('/', function(request, response) {
 //   response.send('Hello World!')
 // })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
